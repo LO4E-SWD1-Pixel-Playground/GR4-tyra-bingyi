9965f2c0bd6190da7a028863397f24d4c64787ea
BingZh <6047656@mborijnland.nl>
2025-06-10 14:18:32 +0200

    Imported IMG/ started with gamepage

--- Pixelplayground/JS/slidehome.js
+++ Pixelplayground/JS/slidehome.js
@@ -1,42 +1,20 @@
-// let slideIndex = 0;
+let slideIndex = 0;
 
-//         function showSlides() {
-//             let slides = document.getElementsByClassName("slide");
+    function showSlides() {
+    let slides = document.getElementsByClassId("slide");
 
-//             for (let i = 0; i < slides.length; i++) {
-//                 slides[i].style.display = "none";
-//             }
-
-//             // loop
-//             slideIndex++;
-//             if (slideIndex > slides.length) {
-//                 slideIndex = 1;
-//             }
-
-//             slides[slideIndex - 1].style.display = "block";
-
-//             setTimeout(showSlides, 3000);
-//         }
-        
-//         showSlides();
-
-let slides = document.querySelectorAll('.slide');
-
-function slideshow() {
-    lastSlide = slideNum;
-    if (slideNum < slides.length -1 ) {
-        slideNum ++;
-    } else {
-        slideNum = 0;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
 
-    slides[lastSlide].style.display = "none";
-    slides[slideNum].style.display = "block";
-    num.innerHTML =`${slideNum + 1} / ${slides.length}`;
-}
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
 
-slides[lastSlide].style.display = "none";
-slides[slideNum].style.display = "block";
-num.innerHTML =`${slideNum + 1} / ${slides.length}`;
-let timer = 3000;
-let slideInter = setInterval(slideshow, timer);
+     slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 3000);
+    }
+        
+    showSlides();
